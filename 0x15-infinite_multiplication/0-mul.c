2e952cf508a2fae0d370d7181f26464ec2f821d0
hfsantiago <35314237+hfsantiago@users.noreply.github.com>
2022-05-18 11:08:47 -0500

    correct nul.c function fixed

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -1,161 +1,149 @@
 #include "holberton.h"
+/* malloc free */
+#include <stdlib.h>
+
 
 /**
- * main - multiplies two positive numbers
- * @argc: argument counter
- * @argv: argument vector
- * Return: 0
+ * initDigitArray - allocates and sets to 0 an array to contain the digits
+ *   of a base 10 number
+ *
+ * @size: array size
+ * Return: pointer to initialized array, or NULL on failure
  */
-int main(int argc, char *argv[])
+unsigned int *initDigitArray(size_t size)
 {
-	char *error = "Error\n";
-	int i;
+	unsigned int *arr = NULL;
+	size_t i;
 
-	if (argc != 3 || !is_a_number(argv[1]) || !is_a_number(argv[2]))
-	{
-		for (i = 0; error[i] != '\0'; i++)
-		{
-			_putchar(error[i]);
-		}
-		exit(98);
-	}
+	arr = malloc(sizeof(unsigned int) * size);
+	if (!arr)
+		return (NULL);
 
-	multiply(argv[1], argv[2]);
-	return (0);
+	for (i = 0; i < size; i++)
+		arr[i] = 0;
+
+	return (arr);
 }
 
-/*corergir lo anterior, porq solo compara el primer caracter*/
 
 /**
- * multiply - Fills an array with the multiplication num_1 times i
- * Arguments
- * @num_1:	string representing the 1st number in 10 base
- * @num_2:  string representing the 2nd number in 10 base
- * Returns - The multiplication
+ * stringIntMultiply - TBD
+ *
+ * @prod_digits: array to store digits of product
+ * @n1_digits: string containing multiplicand digits in ASCII
+ * @n2_digits: string containing multiplier digits in ASCII
+ * @n1_len: amount of digits in multiplicand
+ * @n2_len: amount of digits in multiplier
  */
-
-void multiply(char *num_1, char *num_2)
+void stringIntMultiply(unsigned int *prod_digits, char *n1_digits,
+		       char *n2_digits, size_t n1_len, size_t n2_len)
 {
-	char mul[10][MAX1], answer[MAX1][MAX2];
-	int tag, i, j, len_1, len_2, res, value, pos_1, pos_2, row, z;
+	int i, j, sum;
+	unsigned char digit1, digit2;
 
-	for (len_1 = str_len(num_1), len_2 = str_len(num_2), i = 0; i < 10; i++)
-		for (j = 0; j < MAX1; j++)
-			mul[i][j] = '.';
+	if (prod_digits == NULL || n1_digits == NULL || n2_digits == NULL)
+		return;
 
-	for (i = 0; i < MAX1; i++)
-		for (j = 0; j < MAX2; j++)
-			answer[i][j] = '.';
-
-	/* 1. Creating multiplication table*/
-	for (tag = 0, i = 0; i < 10; i++)
-	{
-		for (j = 0; j <= len_1; j++)
-		{
-			pos_1 = len_1 - j - 1;
-			if (pos_1 >= 0)
-			{	res = ((num_1[pos_1] - 48) * i) + tag;
-				value = res % 10;
-				mul[i][j] = (value + '0');
-				tag = res / 10;
-			}
-			else
-			{	mul[i][j] = (tag != 0) ? tag + 48 : '.';
-				tag = 0;
-			}
-		}
-	}
-
-	/* 2. Creating answer table*/
-	for (j = 0; j < len_2; j++)
-	{	pos_2 = len_2 - j - 1;
-		row = num_2[pos_2] - 48;
-		for (z = 0; z < len_1 + 1; z++)
-			answer[j][z + j] = mul[row][z];
-	}
-	sum_and_print(answer, len_1, len_2);
-}
-
-/**
- * is_a_number - Indicates if a string represents a number in 10 base
- * Arguments
- * @a_str: string representing a number in 10 base
- * Return: 1 if true, 0 if not.
- */
-int is_a_number(char *a_str)
-{
-	while (*a_str != '\0')
-	{
-		if (*a_str < '0' || *a_str > '9')
-			return (_FALSE_);
-		a_str++;
-	}
-	return (_TRUE_);
-}
-
-/**
- * sum_and_print - Generates the sumation and print the results
- * Arguments
- * @a:	matrix with multiplitation tables
- * @len_1: len of string 1
- * @len_2: len os string 2
- * Return: noting.
- */
-void sum_and_print(char a[MAX1][MAX2], int len_1, int len_2)
-{
-	int tag, i, j, sum, value;
-	char total[MAX2], ans[MAX2 - 1];
-	char *ar;
-
-	/* 3. Add each column from left to right */
-	tag = 0;
-	for (j = 0; j <= (len_1 + len_2); j++)
+	for (i = n1_len - 1; i >= 0; i--)
 	{
 		sum = 0;
-		for (i = 0; i < len_2; i++)
-			sum += (a[i][j] >= '0' && a[i][j] <= '9') ? a[i][j] - 48 : 0;
-		value = (sum + tag) % 10;   /* las digit added */
-		total[j] = (value + '0');   /* converted to*/
-		tag = (sum + tag) / 10;
-	}
+		digit1 = n1_digits[i] - '0';
 
-	/* Reverse the string */
-	for (i = 0; i < len_1 + len_2; i++)
-	{
-		ans[i] = total[len_1 + len_2 - 1 - i];
-	}
-	ans[i] = '\0';
-
-	/* remove 0-s from the left side of the string */
-	value = str_len(ans);
-	for (i = 0; i < value; i++)
-	{
-		if (ans[i] == '0')
-			ar = &ans[i + 1];
-		else
+		for (j = n2_len - 1; j >= 0; j--)
 		{
-			ar = &ans[i];
-			i = value;
+			digit2 = n2_digits[j] - '0';
+
+			sum += prod_digits[i + j + 1] + (digit1 * digit2);
+
+			prod_digits[i + j + 1] = sum % 10;
+
+			sum /= 10;
 		}
+
+		if (sum > 0)
+			prod_digits[i + j + 1] += sum;
 	}
-	for (i = 0; ar[i] != '\0'; i++)
-	{
-		_putchar(ar[i]);
-	}
-	putchar('\n');
 }
 
-/**
- * str_len - Calculates the lenght of a string
- * Arguments
- * @a:	matrix with multiplitation tables
- * Return: The lengnth of a.
- */
-int str_len(char *a)
-{
-	int i;
 
-	for (i = 0; a[i] != '\0'; i++)
+/**
+ * stringIsPosInt - validates if string represents a positive integer
+ *
+ * @s: string to test
+ * Return: 1 if true, 0 if false
+ */
+int stringIsPosInt(char *s)
+{
+	size_t i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	}
+
+	return (1);
+}
+
+
+/**
+ * error - error return
+ *
+ * @status: error code to exit with
+ */
+void error(int status)
+{
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
+	exit(status);
+}
+
+
+/**
+ * main - entry point
+ *
+ * @argc: number of commmand line arguments
+ * @argv: array of commmand line arguments
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char **argv)
+{
+	size_t i, av1_len, av2_len, prod_len;
+	unsigned int *prod_digits = NULL;
+
+	if (argc != 3 || !stringIsPosInt(argv[1]) ||
+	    !stringIsPosInt(argv[2]))
+		error(98);
+
+	for (i = 0, av1_len = 0; argv[1][i]; i++)
+		av1_len++;
+
+	for (i = 0, av2_len = 0; argv[2][i]; i++)
+		av2_len++;
+
+	prod_len = av1_len + av2_len;
+	prod_digits = initDigitArray(prod_len);
+	if (prod_digits == NULL)
+		error(98);
+
+	stringIntMultiply(prod_digits, argv[1], argv[2], av1_len, av2_len);
+
+	/* omit leading zeroes */
+	for (i = 0; !prod_digits[i] && i < prod_len; i++)
 	{}
-	return (i);
+
+	if (i == prod_len)
+		_putchar('0');
+
+	for (; i < prod_len; i++)
+		_putchar(prod_digits[i] + '0');
+	_putchar('\n');
+
+	free(prod_digits);
+
+	return (0);
 }
